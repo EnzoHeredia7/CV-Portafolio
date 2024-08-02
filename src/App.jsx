7e755d75619dbeb665010b027e3a4d97c1aea939
EnzoHeredia7 <enzoarielheredia@gmail.com>
2024-08-02 10:13:26 -0300

    subir portafolio

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import "./App.css";
+import Header from './components/header/Header';
+import Home from './components/home/Home'; 
+import About from './components/about/About';
+import Education from './components/education/Education';
+import Contact from './components/contact/Contact';
+import Footer from './components/Footer/Footer';
+
+
+const App = () => {
+  return (
+    <>
+      <Header/>
+      <main className='main'>
+     <Home/>
+     <About/>
+     <Education/>
+     <Contact/>
+     <Footer/>
+      </main>
+    </>
+  )
+}
+
+export default App
+
